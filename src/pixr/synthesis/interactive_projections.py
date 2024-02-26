14175352ba67be1ed3b9d19894dd64d32f06814a
Balthazar Neveu <balthazarneveu@gmail.com>
2024-02-26 01:30:53 +0100

    #2 remove interactivity from pure functions

--- src/pixr/synthesis/interactive_projections.py
+++ src/pixr/synthesis/interactive_projections.py
@@ -6,30 +6,16 @@ from interactive_pipe.data_objects.curves import Curve, SingleCurve
 from pixr.synthesis.raster import get_camera_intrinsics, get_camera_extrinsics, project_3d_to_2d
 
 
-@interactive(
-    yaw_deg=(0., (-180., 180.)),
-    pitch_deg=(0., (-180., 180.)),
-    roll_deg=(0., (-180., 180.)),
-    trans_x=(0., (-10., 10.)),
-    trans_y=(0., (-10., 10.)),
-    trans_z=(0., (-10., 10.))
-)
 def set_camera_parameters(yaw_deg=0., pitch_deg=0., roll_deg=0., trans_x=0., trans_y=0., trans_z=0.) -> torch.Tensor:
     yaw = torch.deg2rad(torch.Tensor([yaw_deg]))
     pitch = torch.deg2rad(torch.Tensor([pitch_deg]))
     roll = torch.deg2rad(torch.Tensor([roll_deg]))
-
     cam_pos = torch.stack([torch.Tensor([trans_x]), torch.Tensor([trans_y]), torch.Tensor([trans_z])])
     return yaw, pitch, roll, cam_pos
 
 
-@interactive(
-    z=(10., (2., 100.)),
-    delta_z=(0., (-5., 5.))
-)
 def generate_3d_scene(z=5, delta_z=0.):
     # [N, 3, xyz]
-
     wc_triangles = torch.Tensor(
         [
             [
@@ -83,21 +69,30 @@ def visualize_2d_scene(cc_triangles: torch.Tensor, w, h) -> Curve:
     return img_scene
 
 
-@interactive_pipeline(gui="mpl", size=(10, 10))
 def projection_pipeline():
-
     wc_triangles = generate_3d_scene()
     yaw, pitch, roll, cam_pos = set_camera_parameters()
     camera_extrinsics = get_camera_extrinsics(yaw, pitch, roll, cam_pos)
     camera_intrinsics, w, h = get_camera_intrinsics()
-    # camera_intrinsics, w, h = get_camera_intrinsics()
     cc_triangles = project_3d_to_2d(wc_triangles, camera_intrinsics, camera_extrinsics)
     img_scene = visualize_2d_scene(cc_triangles, w, h)
     return img_scene
 
 
 def main():
-    projection_pipeline()
+    interactive(
+        z=(10., (2., 100.)),
+        delta_z=(0., (-5., 5.))
+    )(generate_3d_scene)
+    interactive(
+        yaw_deg=(0., (-180., 180.)),
+        pitch_deg=(0., (-180., 180.)),
+        roll_deg=(0., (-180., 180.)),
+        trans_x=(0., (-10., 10.)),
+        trans_y=(0., (-10., 10.)),
+        trans_z=(0., (-10., 10.))
+    )(set_camera_parameters)
+    interactive_pipeline(gui="mpl", size=(10, 10))(projection_pipeline)()
 
 
 if __name__ == '__main__':
