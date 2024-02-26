2bd7efe1593e2e4037d739720defcf065eb30c78
Balthazar Neveu <balthazarneveu@gmail.com>
2024-02-26 02:22:02 +0100

    #2 differentiable forward 3D projections regarding camera parameters Fit camera parameters from 2D projections and known 3D scene (fixed point cloud)

--- src/pixr/synthesis/forward_project.py
+++ src/pixr/synthesis/forward_project.py
@@ -46,5 +46,5 @@ def project_3d_to_2d(wc_triangles: torch.Tensor, camera_intrinsics: torch.Tensor
     cc_triangles = torch.matmul(camera_extrinsics, wc_triangles)
     cc_triangles[:, 1, :] *= -1.  # flip y axis to get a image-like coordinate system
     cc_triangles = torch.matmul(camera_intrinsics, cc_triangles)
-    cc_triangles /= cc_triangles[:, -1:, :]  # pinhole model! normalize by distance
+    cc_triangles = cc_triangles/cc_triangles[:, -1:, :]  # pinhole model! normalize by distance
     return cc_triangles
