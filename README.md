0c9ba0a25cbe7d67a29d4fe19179fa10b7d7ee14
Balthazar Neveu <balthazarneveu@gmail.com>
2024-02-29 01:13:44 +0100

    #2 minor reformat

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 - Program: [MVA Master's degree](https://www.master-mva.com/) class on [3D point clouds](https://www.caor.minesparis.psl.eu/presentation/cours-npm3d/). ENS Paris-Saclay.
 - Author of this code: [Balthazar Neveu](https://www.linkedin.com/in/balthazarneveu/)
 - Study of [ADOP: Approximate Differentiable One-Pixel Point Rendering](https://arxiv.org/pdf/2110.06635.pdf)
+
+
+### Setup
+Local install of `pixr`
+```bash
+pip install -e .
+```
--- src/pixr/synthesis/differentiate_forward_project.py
+++ src/pixr/synthesis/differentiate_forward_project.py
@@ -1,16 +1,19 @@
 from pixr.synthesis.forward_project import get_camera_intrinsics, get_camera_extrinsics, project_3d_to_2d
-from pixr.synthesis.interactive_projections import set_camera_parameters, generate_3d_scene
+from pixr.synthesis.interactive_projections import set_camera_parameters, generate_3d_scene, splat
 
 import torch
 
 
 def main():
     # Fit camera parameters from 2D projections and known 3D scene (fixed point cloud)
-    nice_config = {'yaw_deg': 8.861538461538487, 'pitch_deg': 10.523076923076928, 'roll_deg': -
-                   11.076923076923038, 'trans_x': -1.53846153846154, 'trans_y': 0.7999999999999972, 'trans_z': 0.0}
-    yaw_gt, pitch_gt, roll_gt, cam_pos_gt = set_camera_parameters(**nice_config)
-    proj_point_cloud_gt = forward_chain(yaw_gt, pitch_gt, roll_gt, cam_pos_gt)
-    proj_point_cloud_gt.requires_grad = False
+    with torch.no_grad():
+        nice_config = {
+            'yaw_deg': 8.861538461538487, 'pitch_deg': 10.523076923076928, 'roll_deg': -
+            11.076923076923038, 'trans_x': -1.53846153846154, 'trans_y': 0.7999999999999972, 'trans_z': 0.0}
+        yaw_gt, pitch_gt, roll_gt, cam_pos_gt = set_camera_parameters(**nice_config)
+        proj_point_cloud_gt, splat_image_gt = forward_chain(yaw_gt, pitch_gt, roll_gt, cam_pos_gt)
+        proj_point_cloud_gt.requires_grad = False
+        splat_image_gt.requires_grad = False
     with torch.autograd.set_detect_anomaly(True):
         yaw = torch.tensor([0.], requires_grad=True)
         pitch = torch.tensor([0.], requires_grad=True)
@@ -23,8 +26,9 @@ def main():
         optimizer = torch.optim.Adam([yaw, pitch, roll, cam_pos], lr=0.01)
         for step in range(4000):
             optimizer.zero_grad()
-            proj_point_cloud = forward_chain(yaw, pitch, roll, cam_pos)
-            loss = torch.nn.functional.mse_loss(proj_point_cloud, proj_point_cloud_gt)
+            proj_point_cloud, img = forward_chain(yaw, pitch, roll, cam_pos)
+            # loss = torch.nn.functional.mse_loss(proj_point_cloud, proj_point_cloud_gt)
+            loss = torch.nn.functional.mse_loss(img, splat_image_gt)
             loss.backward()
             optimizer.step()
             if step % 10 == 0:
@@ -38,7 +42,8 @@ def forward_chain(yaw, pitch, roll, cam_pos):
     point_cloud.requires_grad = False
     cam_ext = get_camera_extrinsics(yaw, pitch, roll, cam_pos)
     proj_point_cloud = project_3d_to_2d(point_cloud, cam_int, cam_ext)
-    return proj_point_cloud
+    img = splat(proj_point_cloud, colors, w, h)
+    return proj_point_cloud, img
 
 
 if __name__ == "__main__":
