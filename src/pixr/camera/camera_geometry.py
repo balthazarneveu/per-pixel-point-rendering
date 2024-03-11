e7571fe0a204996e2bb8b4139d8d580bb613a2c5
Balthazar Neveu <balthazarneveu@gmail.com>
2024-03-11 21:42:24 +0100

    #11 revert spin aound center

--- src/pixr/camera/camera_geometry.py
+++ src/pixr/camera/camera_geometry.py
@@ -81,7 +81,7 @@ def get_camera_extrinsics(
     cam_pos: torch.Tensor  # translation
 ) -> torch.Tensor:
     cam_rot = euler_to_rot(yaw, pitch, roll)
-    # cam_pos_rot = torch.matmul(cam_rot, cam_pos)
-    cam_pos_rot = cam_pos
+    cam_pos_rot = torch.matmul(cam_rot, cam_pos)
+    # cam_pos_rot = cam_pos
     camera_ext = torch.cat([cam_rot, cam_pos_rot], dim=1)
     return camera_ext
