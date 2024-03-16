2d5ecbf278f4d74f1d59ee0396fe725f61336bc1
Balthazar Neveu <balthazarneveu@gmail.com>
2024-03-16 16:03:51 +0100

    #9 prepare dataset

--- src/pixr/learning/utils.py
+++ src/pixr/learning/utils.py
@@ -22,25 +22,31 @@ def prepare_data(views_path_list: List[Path]):
     rendered_images = torch.stack(rendered_images)
     camera_intrinsics = torch.stack(camera_intrinsics)
     camera_extrinsics = torch.stack(camera_extrinsics)
+    rendered_images.requires_grad = False
+    camera_intrinsics.requires_grad = False
+    camera_extrinsics.requires_grad = False
     return rendered_images, camera_intrinsics, camera_extrinsics, w, h
 
 
-def split_dataset(rendered_images_size: int, seed: int = 42, ratio_train: float = 0.8) -> Tuple[List[int], List[int]]:
+def split_dataset(number_of_views: int, seed: int = 42, ratio_train: float = 0.8) -> Tuple[List[int], List[int]]:
     assert ratio_train > 0 and ratio_train < 1.
     random.seed(seed)
-    indices = list(range(rendered_images_size))
+    indices = list(range(number_of_views))
     random.shuffle(indices)
-    split = int(rendered_images_size * ratio_train)
+    split = int(number_of_views * ratio_train)
     train_indices = indices[:split]
     val_indices = indices[split:]
     return train_indices, val_indices
 
 
-def prepare_dataset(out_root: Path, name: str,):
+def prepare_dataset(out_root: Path, name: str, seed: int = 42, ratio_train: float = 0.8):
     view_dir = out_root/f"{name}"
     views = sorted(list(view_dir.glob("view_*")))
-    all_rendered_images, camera_intrinsics, camera_extrinsics, w, h = prepare_data(views)
-    all_rendered_images.requires_grad = False
-    # Limit the amount of views for now! -> split train and validation here!
-    # rendered_images = all_rendered_images[:5]
-    rendered_images = all_rendered_images
+    train_indices, valid_indices = split_dataset(len(views))
+    views_train = [views[i] for i in train_indices]
+    views_valid = [views[i] for i in valid_indices]
+    rendered_view_train, camera_intrinsics_train, camera_extrinsics_train, w, h = prepare_data(views_train)
+    rendered_view_valid, camera_intrinsics_valid, camera_extrinsics_valid, w, h = prepare_data(views_valid)
+    # all_rendered_images.requires_grad = False
+    # rendered_images = all_rendered_images
+    return (rendered_view_train, camera_intrinsics_train, camera_extrinsics_train), (rendered_view_valid, camera_intrinsics_valid, camera_extrinsics_valid), (w, h)
