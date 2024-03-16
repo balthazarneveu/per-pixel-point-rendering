fe98b8226303cd4fb544d7c05d0084353edffee6
Balthazar Neveu <balthazarneveu@gmail.com>
2024-03-16 11:20:58 +0100

    #12 add commented code to remove the for loop over pseudo colors... does not seem faster

--- src/pixr/rendering/colors_aggregation.py
+++ src/pixr/rendering/colors_aggregation.py
@@ -23,9 +23,17 @@ def aggregate_colors_fuzzy_depth_test(
     mask = depths <= z_buffer_flat[flat_idx]  # fuzzy depth test
     flat_idx[~mask] = 0  # discarded points are mapped to the dropped element index.
     flat_colors = torch.zeros((1 + h * w, colors.shape[-1]), device=colors.device, dtype=torch.float32)
+    # The per-channel for loop can be avoided with the code below by repeating the indexes on the colors axis..
+    # but this does not seem to be faster
     for ch in range(colors.shape[-1]):
         flat_colors[..., ch] = flat_colors[..., ch].scatter_reduce(0, flat_idx, colors[..., ch], reduce="mean",
                                                                    include_self=False)
+    if False:  # Avoid for loop
+        flat_colors = flat_colors.scatter_reduce(
+            0, flat_idx.unsqueeze(-1).repeat(1, colors.shape[-1]),
+            colors,
+            reduce="mean", include_self=False)
+
     new_colors = flat_colors[1:, :].reshape(h, w, 3)
     print(f"{depths.shape=:}\n , {colors.shape=:}\n{z_buffer_flat.shape=:}")
     print(f"{flat_idx.shape=:}\n {mask.shape=:}\n {min_depth.shape=:}\n {flat_colors.shape=:}\n {new_colors.shape=:}")
