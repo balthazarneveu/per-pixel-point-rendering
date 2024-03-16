891a9302af8ab42672a62fb4ba826525a6b45cca
Balthazar Neveu <balthazarneveu@gmail.com>
2024-03-16 13:44:52 +0100

    #14 opitmize colors per point

--- studies/differentiate_points_splat.py
+++ studies/differentiate_points_splat.py
@@ -14,7 +14,7 @@ from pixr.properties import DEVICE
 import argparse
 
 
-def forward_chain_not_parametric(point_cloud, wc_normals, cam_ext, cam_int, colors, w, h, scale=0):
+def forward_chain_not_parametric(point_cloud, wc_normals, cam_ext, cam_int, colors, w, h, scale=0, no_grad=False):
     # print(point_cloud.device, wc_normals.device, cam_ext.device, cam_int.device, colors.device)
     wc_normals = wc_normals.to(point_cloud.device)
     point_cloud = point_cloud
@@ -26,7 +26,7 @@ def forward_chain_not_parametric(point_cloud, wc_normals, cam_ext, cam_int, colo
         w, h,
         cam_int,
         cc_normals,
-        no_grad=False,
+        no_grad=no_grad,
         scale=scale,
     )
     return img
@@ -72,7 +72,8 @@ def main(out_root=OUT_DIR, name=STAIRCASE, device=DEVICE, show=True, save=False)
     all_rendered_images, camera_intrinsics, camera_extrinsics, w, h = prepare_data(views)
     all_rendered_images.requires_grad = False
     # Limit the amount of views for now! -> split train and validation here!
-    rendered_images = all_rendered_images[:2]
+    # rendered_images = all_rendered_images[:5]
+    rendered_images = all_rendered_images
     rendered_images = rendered_images.to(device)
 
     out_dir = out_root/f"{name}_splat_differentiate_points"
@@ -84,7 +85,7 @@ def main(out_root=OUT_DIR, name=STAIRCASE, device=DEVICE, show=True, save=False)
     wc_points, points_colors, wc_normals = pick_point_cloud_from_triangles(
         wc_triangles, colors, wc_normals,
         # num_samples=20000
-        num_samples=2000
+        num_samples=20000
     )
     # Move data to GPU
     wc_points = wc_points.to(device)
@@ -94,23 +95,26 @@ def main(out_root=OUT_DIR, name=STAIRCASE, device=DEVICE, show=True, save=False)
     color_pred = color_pred.to(device)
     optimizer = torch.optim.Adam([color_pred], lr=0.3)
 
-    scale = 2
+    n_steps = 200+1
+    n_steps = 40+1
+    scales = [0, 1, 2, 3]
     with torch.autograd.set_detect_anomaly(True):
-        for step in range(200+1):
+        for step in range(n_steps):
             optimizer.zero_grad()
             loss = 0.
             # Aggregate the loss over several views
             for batch_idx in range(rendered_images.shape[0]):
                 cam_ext, cam_int = camera_extrinsics[batch_idx, ...], camera_intrinsics[batch_idx, ...]
-                img_pred = forward_chain_not_parametric(
-                    wc_points, wc_normals, cam_ext, cam_int, color_pred, w, h, scale=scale)
-                if scale > 0:
-                    # Downsample the target image using basic average pooling as an AA filter
-                    img_target = torch.nn.functional.avg_pool2d(
-                        rendered_images[batch_idx].permute(2, 0, 1), 2**scale).permute(1, 2, 0)
-                else:
-                    img_target = rendered_images[batch_idx]
-                loss += torch.nn.functional.mse_loss(img_pred, img_target)
+                for scale in scales:
+                    img_pred = forward_chain_not_parametric(
+                        wc_points, wc_normals, cam_ext, cam_int, color_pred, w, h, scale=scale)
+                    if scale > 0:
+                        # Downsample the target image using basic average pooling as an AA filter
+                        img_target = torch.nn.functional.avg_pool2d(
+                            rendered_images[batch_idx].permute(2, 0, 1), 2**scale).permute(1, 2, 0)
+                    else:
+                        img_target = rendered_images[batch_idx]
+                    loss += torch.nn.functional.mse_loss(img_pred, img_target)
             loss.backward()
             optimizer.step()
 
@@ -120,20 +124,36 @@ def main(out_root=OUT_DIR, name=STAIRCASE, device=DEVICE, show=True, save=False)
                 )
             plot_step_flag = step % 10 == 0
             # This is the draft of a validation step
-            if step % 100 == 0 and save:
-                validation_step(
-                    wc_points, wc_normals, camera_extrinsics, camera_intrinsics, w, h, color_pred,
-                    target_img=all_rendered_images, save_path=out_dir, suffix=f"step_{step:05d}")
+            # Plot convergence images
+            if False and step % 100 == 0 and save:
+                for scale in range(3):
+                    validation_step(
+                        wc_points, wc_normals, camera_extrinsics, camera_intrinsics, w, h, color_pred,
+                        target_img=all_rendered_images, save_path=out_dir, suffix=f"step_{step:05d}_scale={scale:02d}",
+                        scale=scale
+                    )
             if plot_step_flag and show:
                 plt.figure()
                 plt.subplot(1, 2, 1)
                 plt.title("Groundtruth")
-                plt.imshow(rendered_images[batch_idx].detach().numpy())
+                plt.imshow(rendered_images[batch_idx].detach().cpu().numpy())
                 plt.subplot(1, 2, 2)
                 print(img_pred.shape)
-                plt.imshow(img_pred.clip(0., 1.).detach().numpy())
+                plt.imshow(img_pred.clip(0., 1.).detach().cpu().numpy())
                 plt.show()
-
+            torch.save({
+                "point_cloud": wc_points,
+                "normals": wc_normals,
+                "colors": color_pred,
+            },
+                out_dir/f"checkpoint_{step:05d}.pt"
+            )
+    for scale in range(3):
+        validation_step(
+            wc_points, wc_normals, camera_extrinsics, camera_intrinsics, w, h, color_pred,
+            target_img=all_rendered_images, save_path=out_dir, suffix=f"step_{step:05d}_scale={scale:02d}",
+            scale=scale
+        )
     plt.figure()
     plt.subplot(1, 2, 1)
     plt.title("Groundtruth")
---
+++ studies/novel_views.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import torch
+from differentiate_points_splat import forward_chain_not_parametric
+from pixr.camera.camera_geometry import set_camera_parameters, get_camera_extrinsics, get_camera_intrinsics
+from interactive_pipe.data_objects.image import Image
+
+
+def main(splat_scene_path):
+    scene_dict = torch.load(splat_scene_path)
+    wc_points = scene_dict["point_cloud"]
+    wc_normals = scene_dict["normals"]
+    color_pred = scene_dict["colors"]
+    scale = 0
+    w, h = 640, 480
+    set_camera_parameters(z=13.)
+    yaw, pitch, roll, cam_pos = set_camera_parameters(trans_z=13.741)
+    camera_extrinsics = get_camera_extrinsics(yaw, pitch, roll, cam_pos)
+    camera_intrinsics, w, h = get_camera_intrinsics()
+    img_pred = forward_chain_not_parametric(
+        wc_points, wc_normals, camera_extrinsics, camera_intrinsics, color_pred, w, h, scale=scale, no_grad=True)
+    Image(img_pred.detach().cpu().numpy()).show()
+
+
+if __name__ == '__main__':
+    splat_scene_path = Path(
+        "/Data/code/per-pixel-point-rendering/__output/staircase_splat_differentiate_points/checkpoint_00040.pt")
+    assert splat_scene_path.exists()
+    main(splat_scene_path)
